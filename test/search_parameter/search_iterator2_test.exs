f7ef81363dbf625d7f1afd0d8d9138cf97a0f376
Riki <luutrunghieubl@gmail.com>
2021-01-14 20:34:45 +0700

    add more unit test

--- test/search_parameter/search_iterator2_test.exs
+++ test/search_parameter/search_iterator2_test.exs
@@ -16,7 +16,7 @@ defmodule SearchParameter.SearchIterator2Test do
   end
 
   describe "next/1" do
-    test "return next search params in happy case" do
+    test "changes page and extension in case of touching limit of page" do
       next_search_params =
         %{
           search_type: :video,
@@ -31,6 +31,75 @@ defmodule SearchParameter.SearchIterator2Test do
       assert next_search_params.topic == "golf"
       assert next_search_params.topic_extension == "news"
       assert next_search_params.page == 1
+    end
+
+    test "changes page, extension and topic in case of touching limit of page and extension" do
+      next_search_params =
+        %{
+          search_type: :video,
+          topic: "baseball",
+          topic_extension: "howto",
+          page: 3
+        }
+        |> CurrentSearch.to_struct()
+        |> SearchIterator2.next()
+
+      assert next_search_params.search_type == :video
+      assert next_search_params.topic == "golf"
+      assert next_search_params.topic_extension == ""
+      assert next_search_params.page == 1
+    end
+
+    test "changes page, extension, topic and search_type in case of touching limit of page, extension and topic" do
+      next_search_params =
+        %{
+          search_type: :video,
+          topic: "golf",
+          topic_extension: "howto",
+          page: 3
+        }
+        |> CurrentSearch.to_struct()
+        |> SearchIterator2.next()
+
+      assert next_search_params.search_type == :channel
+      assert next_search_params.topic == "baseball"
+      assert next_search_params.topic_extension == ""
+      assert next_search_params.page == 1
+    end
+
+    test "return first search in case end of all elements" do
+      next_search_params =
+        %{
+          search_type: :channel,
+          topic: "golf",
+          topic_extension: "howto",
+          page: 3
+        }
+        |> CurrentSearch.to_struct()
+        |> SearchIterator2.next()
+
+      assert next_search_params.search_type == :video
+      assert next_search_params.topic == "baseball"
+      assert next_search_params.topic_extension == ""
+      assert next_search_params.page == 1
+    end
+
+    test "return double next element in case call next two times" do
+      next_search_params =
+      %{
+        search_type: :video,
+        topic: "golf",
+        topic_extension: "tips",
+        page: 3
+      }
+        |> CurrentSearch.to_struct()
+        |> SearchIterator2.next()
+        |> SearchIterator2.next()
+
+      assert next_search_params.search_type == :video
+      assert next_search_params.topic == "golf"
+      assert next_search_params.topic_extension == "news"
+      assert next_search_params.page == 2
     end
   end
 end
